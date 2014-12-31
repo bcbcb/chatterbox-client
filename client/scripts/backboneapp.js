6b366d5285442f0b220856705dbcb984ef806733
bcbcb <bcbcb@users.noreply.github.com>
2014-12-30 21:44:27 -0800

    Backbone implementation is working, but buggy

--- client/scripts/backboneapp.js
+++ client/scripts/backboneapp.js
@@ -1,19 +1,45 @@
 var Message = Backbone.Model.extend({
-
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  defaults: {
+    roomname: 'HackReactor'
+  }
 });
 
 var Messages = Backbone.Collection.extend({
   model: Message,
-
   url: 'https://api.parse.com/1/classes/chatterbox',
 
   loadMessages: function () {
-    this.fetch({data: {order: '-createdAt'}});
+    this.fetch({data: {order: '-createdAt', where: {'roomname': 'HackReactor'}}});
   },
 
   parse: function (response) {
     var messages = response.results;
-    return messages;
+    var results = [];
+    for (var i = messages.length-1; i > 0; i--) {
+      results.push(messages[i]);
+    }
+    return results;
+  }
+
+});
+
+var FormView = Backbone.View.extend({
+
+  events: {
+    'click #send': 'handleSubmit'
+  },
+
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var $text = this.$('#message');
+
+    this.collection.create({
+      username: window.location.search.split('=')[1],
+      text: $text.val(),
+      roomname: 'HackReactor'
+    });
+    $text.val('');
   }
 
 });
@@ -41,5 +67,23 @@ var MessagesView = Backbone.View.extend({
     this.$el.prepend(messageView.render());
   }
 
+});
+
+/*
+
+var RoomsView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+  },
+
+  render: function() {
+    this.collection.forEach(this.renderRooms, this);
+  },
+
+  renderRooms: function (room) {
+    var roomView = new RoomView({model: room});
+    this.$el.append(roomView.render());
+  }
 
 });
+*/
